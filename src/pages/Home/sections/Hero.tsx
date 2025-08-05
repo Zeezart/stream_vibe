05b1b8bc4b2e6b1efec999db866e7023137b8e06
Zeezart <azeezhazeezah010@gmail.com>
2025-08-05 21:42:25 +0100

    test

--- src/pages/Home/sections/Hero.tsx
+++ src/pages/Home/sections/Hero.tsx
@@ -26,7 +26,7 @@ function Hero() {
 
                 <Box textAlign="center" width={{base:"100%", md:"75%"}} m="0 auto">
                     <Box m="1.5rem 0">
-                        <Text>Hello testing please work</Text>
+                        <Text>Hello testing please work..normal git</Text>
                         <Text fontSize={{base:"2rem", md:"3rem"}} color="#fff" fontWeight="bold">The Best Streaming Experience</Text>
                         <Text fontSize="0.8rem" color="#999">StreamVibe is the best streaming experience for watching your favorite movies and shows on demand, anytime, anywhere. With StreamVibe, you can enjoy a wide variety of content, including the latest blockbusters, classic movies, popular TV shows, and more. 
                             You can also create your own watchlists, so you can easily find the content you want to watch.</Text>
